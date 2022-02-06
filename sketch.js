3c4584741332bcb6bea2cbc105b2a94d4b4d9d14
Drewe4401 <70233363+Drewe4401@users.noreply.github.com>
2022-02-05 20:07:30 -0600

    commit

--- sketch.js
+++ sketch.js
@@ -1,7 +1,94 @@
+let character;
+let character2;
+let character3;
+let character4;
+
+function preload() {
+  spriteSheet = loadImage("SpelunkyGuy.png");
+  spriteSheet2 = loadImage("green.png");
+  spriteSheet3 = loadImage("ninja.png");
+  spriteSheet4 = loadImage("robot.png");
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1200, 1200);
+  imageMode(CENTER);
+  character = new Character(spriteSheet, 200, 300);
+  character2 = new Character(spriteSheet2, 100, 500);
+  character3 = new Character(spriteSheet3, 400, 500);
+  character4 = new Character(spriteSheet4, 500, 700);
+}
+
+function keyPressed(){
+  if(keyCode == RIGHT_ARROW){
+    character.go(1);
+    character2.go(1);
+    character3.go(1);
+    character4.go(1);
+  }
+  else if(keyCode == LEFT_ARROW){
+    character.go(-1);
+    character2.go(-1);
+    character3.go(-1);
+    character4.go(-1);
+  }
+}
+
+function keyReleased(){
+    character.stop();
+    character2.stop();
+    character3.stop();
+    character4.stop();
 }
 
 function draw() {
-  background(220, 0, 40);
+ background(255,255,255);
+ character.draw();
+ character2.draw();
+ character3.draw();
+ character4.draw();
 }
+
+
+class Character{
+constructor(spriteSheet, x, y){
+  this.spriteSheet = spriteSheet;
+  this.sx = 0;
+  this.x = x;
+  this.y = y;
+  this.move = 0;
+  this.facing = 1;
+}
+  draw() {
+    push();
+    translate(this.x,this.y);
+    scale(this.facing,1);
+   
+   if(this.move == 0){
+    image(this.spriteSheet, 0, 0, 200, 200, 0, 0, 80, 80);
+   }
+   else{
+     image(this.spriteSheet, 0, 0, 200, 200, 80 * (this.sx+1), 0, 80, 80);
+   }
+    
+    if(frameCount % 5 == 0){
+    this.sx= (this.sx + 1) % 8;
+    }
+    this.x += 2 * this.move;
+    pop();
+  }
+
+  go(direction) {
+    this.move = direction;
+    this.facing = direction;
+    this.sx = 3;
+  }
+
+  stop() {
+    this.move = 0;
+  }
+}
+
+  
+
+
